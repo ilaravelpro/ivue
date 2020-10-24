77feda7fdca53222a4a670d6c7310a65499cd7dd
bitcoder <me@iamir.net>
2020-10-24 21:38:27 +0330

    up

--- src/js/views/layouts/forms/FormTabs.vue
+++ src/js/views/layouts/forms/FormTabs.vue
@@ -2,14 +2,14 @@
     <div :class="_style">
         <ul class="nav" :class="_style_nav" role="tablist"
             :aria-orientation="_orientation">
-            <li v-for="(tab, index) in Object.values(tabs)" class="nav-item">
+            <li v-for="(tab, index) in Object.values(getTabs)" class="nav-item">
                 <a class="nav-link" :class="index === 0 ? 'active' :''" :id="`${_id(tab.name)}_tab`" data-toggle="tab"
                    :href="`#${_id(tab.name)}`" role="tab" :aria-controls="_id(tab.name)" aria-selected="true">{{
                     tab.title }}</a>
             </li>
         </ul>
         <div class="tab-content" :class="_style_contents">
-            <div v-for="(tab, index) in  Object.values(tabs)" class="tab-pane fade show p-2" :class="index === 0 ? 'active ' :''"
+            <div v-for="(tab, index) in  Object.values(getTabs)" class="tab-pane fade show p-2" :class="index === 0 ? 'active ' :''"
                  :id="_id(tab.name)" role="tabpanel" :aria-labelledby="`${_id(tab.name)}_tab`">
                 <button v-if="isBtnView(tab)" class="btn btn-warning" @click="setView(tab)">Load Tab</button>
                 <slot v-if="$scopedSlots[`tab.${tab.name}`]" :name="`tab.${tab.name}`" v-bind:tab="tab" v-bind:namespace="storeNamespace"></slot>
@@ -24,10 +24,12 @@
 </template>
 
 <script>
+    import GlobalField from "../../../handel/functions/field/global.func";
+
     export default {
         name: "i-form-tabs",
         props: {
-            tabs: [Object, Array],
+            tabs: [Object, Array, Function],
             css: [String, Object, Array],
             id: String,
             vertical: {
@@ -46,6 +48,10 @@
           }
         },
         computed: {
+            ...GlobalField.computed(),
+            getTabs() {
+                return typeof(this.tabs) == 'function' ? this.tabs(this) : this.tabs;
+            },
             _style() {
                 var $style = this.getStyle('main') || '';
                 if (this.vertical) $style += $style === '' ? 'row col-12' : ' row col-12';
@@ -67,6 +73,7 @@
             },
         },
         methods: {
+            ...GlobalField.methods(),
             getStyle(name) {
                 if (name === 'main' && typeof (this.css) === 'string') return this.css;
                 else if (typeof (this.css) === 'object') return iPath(this.css, name);
