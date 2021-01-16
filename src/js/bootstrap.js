4f331706c3818f244f30215027f149f9d7868b3f
bitcoder <me@iamir.net>
2021-01-16 18:52:46 +0330

    Update bootstrap.js

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -5,9 +5,7 @@ try {
     window.Popper = require('popper.js').default;
     window.$ = window.jQuery = require('jquery');
     require('bootstrap').default;
-} catch (e) {
-    console.log(e)
-}
+} catch (e) {}
 window.purify = o => JSON.parse(JSON.stringify(o))
 Vue.prototype.$eventHub = new Vue();
 
